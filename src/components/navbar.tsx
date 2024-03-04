a1fa7ce985eb56d6af43ffea3b75cfa2bee97a44
JFMai90 <jasonmaif@gmail.com>
2024-03-04 00:54:31 -0800

    turn reload link into button and set delay to .4s

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,9 @@ import { RefreshCwIcon } from "lucide-react"
 export default function Navbar() {
 
   const reload = () => {
-    window.location.reload();
+    setTimeout(() => {
+      window.location.reload();
+    }, 400);
   }
 
   return <div className="p-8 gap-4 md:gap-8 flex flex-row justify-center items-center space-around">
@@ -15,13 +17,12 @@ export default function Navbar() {
     >
       <div className="text-4xl">💩</div>
     </Link>
-    <Link
-      href="#"
+    <button
       className="py-4 px-8 border-4 border-solid hover:text-yellow-950 hover:border-yellow-950 border-yellow-900 text-yellow-900  rounded-full"
       onClick={reload}
     >
       <RefreshCwIcon />
-    </Link>
+    </button>
     <Link
       href="/about"
       className="py-4 px-8 border-4 border-solid hover:text-yellow-950 hover:border-yellow-950 border-yellow-900 text-yellow-900 rounded-full font-bold"
