b3efb5d4ad3e8523ef08aece35b260de58664f8a
JFMai90 <jasonmaif@gmail.com>
2024-03-03 21:03:41 -0800

    style loading component

--- src/app/dog/loading.tsx
+++ src/app/dog/loading.tsx
@@ -1,7 +1,7 @@
 "use client"
 export default function Loading() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 text-yellow-800">
       Loading...
     </main>
   )
--- src/app/dog/page.tsx
+++ src/app/dog/page.tsx
@@ -8,7 +8,7 @@ const getOneRandomDogImage = async () => {
 export default async function Dog() {
   const dog = await getOneRandomDogImage()
   return (
-    <main className="flex flex-col items-center p-8 gap-4">
+    <main className="flex flex-col items-center p-8 gap-4 text-yellow-800">
       <Image src={dog?.message} alt="Dog" width={300} height={300} className="border-0 border-solid rounded-xl" />
     </main>
   )
