884c6884ff4263e93b6fca464f77f134b228fb61
JFMai90 <jasonmaif@gmail.com>
2024-03-04 00:45:54 -0800

    add routing to emoji, restructure reload functionality

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,25 +1,25 @@
 "use client";
 import Link from "next/link";
 import { RefreshCwIcon } from "lucide-react"
-import { useRouter } from "next/navigation"
 
 export default function Navbar() {
-  const router = useRouter()
 
-  const reloadAndNavigate = () => {
-    router.push('/')
-    setTimeout(() => {
-      window.location.reload();
-    }, 700);
+  const reload = () => {
+    window.location.reload();
   }
 
   return <div className="p-8 gap-4 md:gap-8 flex flex-row justify-center items-center space-around">
-    <div className="text-4xl">💩</div>
+    <Link
+      href="/"
+      className="py-4 px-8"
+    >
+      <div className="text-4xl">💩</div>
+    </Link>
     <Link
       href="/"
       className="py-4 px-8 border-4 border-solid hover:text-yellow-950 hover:border-yellow-950 border-yellow-900 text-yellow-900  rounded-full"
 
-      onClick={reloadAndNavigate}
+      onClick={reload}
     >
       <RefreshCwIcon />
     </Link>
